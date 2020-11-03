cba76de215782509ed717670a62fc13bdb61eed6
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2020-11-02 19:04:52 -0600

    Created user model with devise and created post model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      flash.notice = "Post successfully created!"
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+end
