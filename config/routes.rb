3d918cd8614ef10a116c44a575ea481af8ddb762
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2020-11-03 14:29:29 -0600

    added user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :posts
+  resources :users, only: [:index, :show]
 
   root 'posts#index'
 end
